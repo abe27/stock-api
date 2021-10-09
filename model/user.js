f251e16bf1768a05e0bb7406d75af953336adf20
Parzival Yuenyang <krumii.it@gmail.com>
2021-10-09 13:33:06 +0700

    add route

--- model/user.js
+++ model/user.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     first_name: { type: String, default: null },
     last_name: { type: String, default: null },
-    email: { type: String, unique: true },
-    password: { type: String },
+    email: { type: String, unique: true, trim: true },
+    password: { type: String, trim: true },
     token: { type: String },
 });
 
